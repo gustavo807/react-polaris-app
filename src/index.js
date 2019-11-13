a74424f0b815fac8c7ccbb8c35542cdb501a210f
Gustavo Alberto <gustavo.garcia@mexmax-internet.com>
2019-11-13 17:27:33 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '@shopify/polaris/styles.css';
+import {AppProvider} from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function WrappedApp() {
+    return (
+      <AppProvider i18n={enTranslations}>
+        <App />
+      </AppProvider>
+    );
+  }
+
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
+
+
